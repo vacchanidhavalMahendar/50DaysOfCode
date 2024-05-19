d59a2e7dc1f864fbc6f3142ac227589b27bdd5fb
vacchanidhavalMahendar <dhavalvacchani1111@gmail.com>
2024-05-19 17:44:38 +0530

    this is my Second Commit

---
+++ src/Day2_LogicalBuilding/MaximumElementArray.java
@@ -0,0 +1,21 @@
+package Day2_LogicalBuilding;
+
+import java.util.Arrays;
+
+//Question 4:WAP to find the maximum element in an array
+public class MaximumElementArray {
+    public static void main(String[] args) {
+
+        int arr[]={10,20,50,60,1880,30,40,190};
+        int max=0;
+        for(int i=0;i< arr.length;i++){
+
+            if(arr[i]>max){
+                max=arr[i];
+            }
+
+
+        }
+        System.out.println("The Maximum Element in an array"+ " " + max);
+    }
+}
---
+++ src/Day2_LogicalBuilding/ReverseAnArray.java
@@ -0,0 +1,16 @@
+package Day2_LogicalBuilding;
+
+//Question 3:WAP to function to reverse an array
+public class ReverseAnArray {
+    public static void main(String[] args) {
+
+        int arr[]={1,5,6,7,8,9,10,11,12,13,14,15};
+
+        for(int i=arr.length-1; i>=0;i--){
+
+            System.out.print(" "+arr[i])  ;
+
+        }
+        System.out.println(" ");
+    }
+}
